989b29ccc431e54804bf4895786e4085453d9801
ltasneem <lubaba070@gmail.com>
2017-06-20 14:34:28 -0400

    Randomness change activity

---
+++ src/Randomness.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+
+public class Randomness {
+	 public static void main(String[] args)
+	    {
+	        output("Generate 10 random integers between 0 and 6");
+	        Random rnd = new Random();
+	        
+	        for (int i = 1; i <= 10; ++i)
+	        {
+	          int randomInt = 1 + rnd.nextInt(6);
+	          output("Generated number: " + randomInt);
+	        }
+	    
+	        output("Done.");
+	        
+	        
+	        /*Randomness Change Activity */
+	        
+	        output("Generate 10 random integers between 1 and 6");
+
+	        //Random rnd = new Random();
+	        
+	        for (int i = 1; i <= 10; ++i)
+	        {
+	          int randomInt = 1 + rnd.nextInt(95);
+	          System.out.println("Generated number: " + randomInt);
+	        }
+	    
+	        System.out.println("Done.");
+
+	    }
+	  
+	  private static void output(String aMessage)
+	  {
+	    System.out.println(aMessage);
+	  }
+
+}
